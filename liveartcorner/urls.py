4c66006aa28df7aa49d0e1e32f3fdbb67a676438
GmGith <87491753+GmGith@users.noreply.github.com>
2024-06-23 19:29:57 +0200

    Fixing Docker staticfile

--- liveartcorner/urls.py
+++ liveartcorner/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 #from base.views import home
-from cart import views
+#from cart import views
 from django.views.static import serve
 
 
